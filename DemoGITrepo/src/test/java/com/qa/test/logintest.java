9095887bd578a91a999147e9fcd014df88d5b80d
vasanthkumar.ln <vasanthkumar.ln@gmail.com>
2022-08-01 17:22:29 +0530

    logintest_fix

--- DemoGITrepo/src/test/java/com/qa/test/logintest.java
+++ DemoGITrepo/src/test/java/com/qa/test/logintest.java
@@ -1,5 +1,12 @@
 package com.qa.test;
 
-public class logintest {
+import org.testng.annotations.Test;
 
+public class logintest {
+	
+	@Test
+	public void test() {
+	System.out.println("testng case");
+
+	}
 }
